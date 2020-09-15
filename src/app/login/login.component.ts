f24c7345681219d327e04641fc1f6bc9e42ea118
giuliano.truant <giulitruant_26@hotmail.com>
2020-09-14 23:57:37 -0300

    Login init

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  form = new FormGroup({
+    user : new FormControl('', Validators.required),
+    pass : new FormControl('', Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit(){
+    if(this.form.invalid)
+    {
+      return;
+    }
+
+    console.dir(this.form.value);
+
   }
 
 }
